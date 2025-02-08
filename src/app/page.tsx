40502b157aa63024d67894674db7d025b6acd270
Mohammad Erfun Ghodoosi <erfunghodoosi@gmail.com>
2025-02-08 20:05:59 +0330

    feat: navbar component base design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "@/components/layout/navbar";
+
 export default function Page() {
   return (
-    <main>
+    <main className="container">
+      <Navbar />
       <h1 className="heading-1 container">سلام!</h1>
     </main>
   );
---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,43 @@
+import { LoginCurve, SearchNormal1, ShoppingCart } from "iconsax-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="flex justify-between items-center py-32">
+      <div>
+        <Image
+          src="/logo/header.svg"
+          alt="costomme logo"
+          width={200}
+          height={60}
+        />
+      </div>
+      <div>
+        <button className="w-[596px] rounded-12 h-44 border-gray-4 border flex justify-between items-center text-primary body-4 px-24">
+          <span>جستجو</span>
+          <SearchNormal1 size={24} color="currentColor" />
+        </button>
+      </div>
+      <div className="flex gap-48">
+        <Link
+          href={"#"}
+          className="text-gray-10 flex items-center gap-1 hover:text-primary-shade-6 transition-all"
+        >
+          <LoginCurve size={24} color="currentColor" />
+          <span>ورود و ثبت نام</span>
+        </Link>
+        <Link
+          href={"#"}
+          className="text-gray-10 flex items-center gap-1 hover:text-primary-shade-6 transition-all"
+        >
+          <ShoppingCart size={24} color="currentColor" />
+          <span>سبد خرید</span>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
