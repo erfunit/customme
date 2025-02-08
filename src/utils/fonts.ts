8235d0c84b93a736565bce9d13dfc277fd3c87f8
Mohammad Erfun Ghodoosi <erfunghodoosi@gmail.com>
2025-02-08 19:41:43 +0330

    init: nextjs 15 project and style guides

---
+++ src/utils/fonts.ts
@@ -0,0 +1,52 @@
+import localFont from "next/font/local";
+
+export const iranYekanX = localFont({
+  src: [
+    {
+      path: "../../public/fonts/IranYekanX/Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/IranYekanX/UltraLight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/IranYekanX/Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/IranYekanX/Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/IranYekanX/Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/IranYekanX/DemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/IranYekanX/Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/IranYekanX/ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/IranYekanX/Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-iran-yekan-x",
+});
